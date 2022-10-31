13875832d321bb4414cf99158e2340e699c06e2c
Peratham <75246140+perathambkk@users.noreply.github.com>
2022-10-31 23:52:30 +0700

    Create pi_estimate.py

---
+++ pi_estimate.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --
+"""การประมาณค่าพายด้วยวิธี Monte Carlo
+"""
+import random
+import math
+num_samples = 100000 # จำนวนตัวอย่าง
+r = 5.0 # รัศมีวงกลมหรือครึ่งหนึ่งของความยาวด้านของสี่เหลี่ยมจัตุรัส
+a, b = -r, r # ช่วง [a, b] คือ [-r, r]
+in_circle = 0
+for i in range(num_samples): # สุ่มไปเรื่อยๆ
+    x = random.uniform(a, b)
+    y = random.uniform(a, b)
+    if x**2 + y**2 <= r**2: # หากอยู่ในวงกลมก็นับ
+        in_circle += 1
+print('[Result] pi estimation: {0:.5f}'.format(4*float(in_circle)/num_samples)) # แสดงค่าพายที่ประมาณจากการทดลอง
